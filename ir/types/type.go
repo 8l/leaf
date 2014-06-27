837a00433f1b7e3131807df526e9f838f2290006
He Liu <liulonnie@gmail.com>
2014-06-27 01:20:33 -0700

    type equal

--- ir/types/type.go
+++ ir/types/type.go
@@ -12,3 +12,17 @@ func TypeStr(t Type) string {
 
 	return t.String()
 }
+
+func Equals(t1, t2 Type) bool {
+	b1 := Marsh(t1)
+	b2 := Marsh(t2)
+	if len(b1) != len(b2) {
+		return false
+	}
+	for i := range b1 {
+		if b1[i] != b2[i] {
+			return false
+		}
+	}
+	return true
+}
