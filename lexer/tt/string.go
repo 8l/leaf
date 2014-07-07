486b15ef6d607db1c267911f32d31fdffc736826
He Liu <liulonnie@gmail.com>
2014-07-06 18:59:28 -0700

    t

--- lexer/tt/string.go
+++ lexer/tt/string.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-var tokenStr = map[T]string{
+var strs = map[T]string{
 	Illegal: "Illegal",
 	EOF:     "EOF",
 	Comment: "Comment",
@@ -89,7 +89,7 @@ var tokenStr = map[T]string{
 }
 
 func (t T) String() string {
-	if s, found := tokenStr[t]; found {
+	if s, found := strs[t]; found {
 		return s
 	}
 	return fmt.Sprintf("<na-%d>", int(t))
@@ -98,7 +98,7 @@ func (t T) String() string {
 var keywords = func() map[string]T {
 	ret := make(map[string]T)
 	for i := keywordBegin + 1; i < keywordEnd; i++ {
-		s := tokenStr[i]
+		s := strs[i]
 		ret[s] = i
 	}
 	return ret
