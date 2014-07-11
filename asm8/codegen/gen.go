4f6240354c52798d3140876195f755dc331246f9
He Liu <liulonnie@gmail.com>
2014-07-11 00:43:29 -0700

    codegen example

---
+++ asm8/codegen/gen.go
@@ -0,0 +1,18 @@
+package codegen
+
+func example() {
+	/*
+		b := new(build)
+		f1 := b.DeclFunc("func1")
+
+		f1.Label() // add a new label
+		i1 := f1.Inst(inst.Jinst(xxx))
+		i1.Bind("func2")
+
+		f2 := b.DeclFunc("func2")
+
+
+		b.Layout()
+		b.LinkLabels()
+	*/
+}
