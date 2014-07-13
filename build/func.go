7eb9bf5593624198991858e73476ec0effd4d9bf
He Liu <liulonnie@gmail.com>
2014-07-12 21:01:29 -0700

    func

--- build/func.go
+++ build/func.go
@@ -1,4 +1,19 @@
 package build
 
+import (
+	"e8vm.net/e8/inst"
+)
+
+// Func is a continuous memory segment that saves executable instructions.
 type Func struct {
+	name   string
+	lines  []*Line
+	labels map[string]int
+}
+
+// Line is an instruction where the lable might be not binded. Each
+// instruction always take exactly one uint32.
+type Line struct {
+	i     inst.Inst
+	label string // how the label is applied depends on the inst op
 }
