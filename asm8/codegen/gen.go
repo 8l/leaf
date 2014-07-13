855978d96ae90cf7582ea2278fa1e1f74d4a5e89
He Liu <liulonnie@gmail.com>
2014-07-13 09:41:19 -0700

    .

--- asm8/codegen/gen.go
+++ asm8/codegen/gen.go
@@ -1,19 +1,48 @@
 package codegen
 
-func example() {
-	/*
-		b := new(build)
-		f1 := b.DeclFunc("func1")
+import (
+	"e8vm.net/leaf/asm8/ast"
+	"e8vm.net/leaf/asm8/build"
+)
 
-		f1.Label() // add a new label
-		i1 := f1.Inst(inst.Jinst(xxx))
-		i1.BindLabel("func2")
+// Gen generates the code into a build.
+type Gen struct {
+	build *build.Build
+	prog  *ast.Program
 
-		f2 := b.DeclFunc("func2")
+	errors []error
+}
 
-		b.Build()
+// NewGen creates a new code generator that
+// generates the code into a build.
+func NewGen(b *build.Build, p *ast.Program) *Gen {
+	ret := new(Gen)
+	ret.build = b
+	ret.prog = p
 
-		b.Layout()
-		b.LinkLabels()
-	*/
+	return ret
+}
+
+// Gen performs the code generation.
+func (g *Gen) Gen() []error {
+	for _, d := range g.prog.Decls {
+		g.declare(d)
+	}
+
+	if len(g.errors) > 0 {
+		return g.errors
+	}
+
+	return g.errors
+}
+
+func (g *Gen) declare(d ast.Decl) {
+	switch d.(type) {
+	case *ast.Const:
+		panic("todo")
+	case *ast.Var:
+		panic("todo")
+	case *ast.Func:
+		panic("todo")
+	}
 }
